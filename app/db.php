2b21d30fc30f0163679b536f08ffa15e22776860
Unknown <axelfertinel@gmail.com>
2018-06-25 11:57:51 +0200

    db

--- app/db.php
+++ app/db.php
@@ -1,6 +1,6 @@
 <?php
 
 define("APP_DB_HOST","localhost");
-define("APP_DB_NAME","MVC");
-define("APP_DB_USER","AxelFertinel");
-define("APP_DB_PWD", "fertis5821");
+define("APP_DB_NAME","BDD");
+define("APP_DB_USER","user");
+define("APP_DB_PWD", "pwd");
